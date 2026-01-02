c67560c6a340f8682a4a9e986bbc40100d4d7eed
Live Experience Media <info@mbswellnessgroup.org>
2026-01-02 12:52:43 -0800

    Replace placeholder favicon with monogram (svg + fallback)

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
     // Static assets
     eleventyConfig.addPassthroughCopy("styles.css");
     eleventyConfig.addPassthroughCopy("1440-logo.svg");
+    eleventyConfig.addPassthroughCopy("favicon.svg");
     eleventyConfig.addPassthroughCopy("favicon.ico");
 
     return {
